40d00d42647980f619cb0fde617ff769b73da578
OleksandrFliagin <samme0062@gamil.com>
2023-11-22 21:58:03 +0200

    init

---
+++ src/test/java/com/rst/tableservice/TableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rst.tableservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
