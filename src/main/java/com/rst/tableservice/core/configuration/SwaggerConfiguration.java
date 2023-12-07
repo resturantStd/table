b13baa49a2121d4d505d305996a0b8d83fb8cf77
OleksandrFliagin <samme0062@gamil.com>
2023-12-07 10:52:26 +0200

    The commit refers to the removal of a few superfluous lines from SwaggerConfiguration.java. Also, application.yaml is updated with additional server-related lines.

--- src/main/java/com/rst/tableservice/core/configuration/SwaggerConfiguration.java
+++ src/main/java/com/rst/tableservice/core/configuration/SwaggerConfiguration.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
-
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +14,6 @@ import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
-
 
     @Bean
     public GroupedOpenApi publicUserApi() {
@@ -31,11 +29,8 @@ public class SwaggerConfiguration {
         return new OpenAPI().info(new Info().title("Application API")
                         .version(appVersion)
                         .description(appDescription)
-                        .license(new License().name("Apache 2.0")
-                                .url("http://springdoc.org"))
-                        .contact(new Contact().name("username")
-                                .email("test@gmail.com")))
-                .servers(List.of(new Server().url("http://localhost:8092")
-                                .description("Dev service")));
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                        .contact(new Contact().name("username").email("test@gmail.com")))
+                .servers(List.of(new Server().url("http://localhost:8092").description("Dev service")));
     }
 }
